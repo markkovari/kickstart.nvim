a4f1a03756f215ff6d4b68c59e72b1ba9cb50c49
Márk Kővári <kovarimarkofficial@gmail.com>
2024-12-27 02:56:22 +0100

    chore(oil): add oil for file management
    Signed-off-by: Márk Kővári <kovarimarkofficial@gmail.com>

--- init.lua
+++ init.lua
@@ -167,6 +167,10 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
+-- Oil - or open files
+
+vim.keymap.set('n', '<leader>o', '<cmd>Oil<CR>', { desc = 'Open Oil - do file things' })
+
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.
@@ -933,6 +937,7 @@ require('lazy').setup({
   -- require 'kickstart.plugins.lint',
   -- require 'kickstart.plugins.autopairs',
   -- require 'kickstart.plugins.neo-tree',
+  require 'kickstart.plugins.oil',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`

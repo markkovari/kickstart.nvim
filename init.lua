616c4f91711d240603f2ae81d4f0b1e82ebcd404
markkovari <kovarimarkofficial@gmail.com>
2025-06-19 13:09:31 +0200

    chore(): add cmdline
    Signed-off-by: markkovari <kovarimarkofficial@gmail.com>

--- init.lua
+++ init.lua
@@ -478,6 +478,21 @@ require('lazy').setup({
         { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
       },
     },
+    {
+      'folke/noice.nvim',
+      event = 'VeryLazy',
+      opts = {
+        -- add any options here
+      },
+      dependencies = {
+        -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+        'MunifTanjim/nui.nvim',
+        -- OPTIONAL:
+        --   `nvim-notify` is only needed, if you want to use the notification view.
+        --   If not available, we use `mini` as the fallback
+        'rcarriga/nvim-notify',
+      },
+    },
   },
   {
     -- Main LSP Configuration
